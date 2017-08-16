4719eadca8351aed13f7120c5409870498a6a9f6
Adam Bossy-Mendoza <adam@ip-192-168-128-94.us-west-1.compute.internal>
2017-08-16 12:56:42 -0700

    First commit!

---
+++ RingoSpriteKit/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  RingoSpriteKit
+//
+//  Created by Adam Bossy-Mendoza on 8/16/17.
+//  Copyright © 2017 Patreon. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
