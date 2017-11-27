b5452e0751e7d89c5843b59129d24edb2c662f50
Adam Bossy <adambossy@gmail.com>
2017-11-27 08:29:33 -0700

    Create RingoRenderer project.

---
+++ RingoRenderer/RingoRenderer/RingoRenderer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RingoRenderer
+//
+//  Created by Adam Bossy-Mendoza on 11/27/17.
+//  Copyright © 2017 Patreon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RingoRenderer/RingoRenderer/RingoRenderer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RingoRenderer
+//
+//  Created by Adam Bossy-Mendoza on 11/27/17.
+//  Copyright © 2017 Patreon. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
