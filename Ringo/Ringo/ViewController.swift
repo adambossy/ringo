84fb8a3abcdf9a7a48ba899e6767b1f565af03b5
Adam Bossy <adambossy@gmail.com>
2018-02-19 14:41:16 -0800

    It works! Loaded Playground files and rendering worked on the first try.
    Rests aren't loading properly, and the entire viewport is slightly smushed.

---
+++ Ringo/Ringo/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Ringo
+//
+//  Created by Adam Bossy-Mendoza on 2/19/18.
+//  Copyright © 2018 Bossy Software. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let sceneWidth: CGFloat = 800
+        let sceneHeight: CGFloat = 1200
+        let scene = SheetMusicScene(song: Rush_TomSawyer, size: CGSize(width: sceneWidth, height: sceneHeight))
+        
+        // Playground sturf
+        let sceneView = SKView(frame: CGRect(x: 0, y: 0, width: sceneWidth, height: sceneHeight))
+        sceneView.showsFPS = true
+        sceneView.presentScene(scene)
+        self.view = sceneView
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
